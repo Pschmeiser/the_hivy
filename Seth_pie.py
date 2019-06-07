09466f38606113dd456f9d187d40890067778af0
shillis17 <shillis17@live.com>
2019-06-07 09:44:13 -0600

    made my Seth_pie.py

---
+++ Seth_pie.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 09:41:35 2019
+
+@author: seth
+"""
+
+import pandas as pd
+
+df = pd.read_csv('data/merged_data.csv')
+df.head()
