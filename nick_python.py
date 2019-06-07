2a2c41d5b72865f80e9f3651d856d914c569db9a
Nicolas Jacobsohn <nicolasjacobsohn@Nicolass-MacBook-Pro.local>
2019-06-07 12:59:23 -0600

    more functions

--- nick_python.py
+++ nick_python.py
@@ -1,30 +1,28 @@
 import numpy as np
 import pandas as pd
-import sklearn
 from sklearn import datasets
 from sklearn import linear_model
 from sklearn import metrics
+from sklearn import model_selection
 from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import scipy.stats as stats
-import random
-
-
+from yellowbrick.regressor import ResidualsPlot
 import seaborn as sns
+
 import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
+sns.set(style="ticks")
 font = {'size'   : 12}
 matplotlib.rc('font', **font)
-sns.set(style="ticks")
 def read_data():
     df = pd.read_csv("data/merged_data.csv")
     df.fillna(0, inplace = True)
     df.drop(set(df[df["HIVdiagnoses"] == 0].index), axis=0, inplace=True)
     cols = ["HIVdiagnoses", "HIVprevalence", "PLHIV", "Population"]
-    X = pd.DataFrame(index=df["ADULTMEN"].index, columns=cols)
+    X = pd.DataFrame(index=df["HIVdiagnoses"].index, columns=cols)
     for col in cols:
         X[col] = df[col]
     y = np.array(df["HIVincidence"])
@@ -45,13 +43,13 @@ def plot_alpha_vals(X_train, X_test, y_train, y_test, alphas):
     ax.plot(alphas, mse_test)
 
 def plot_residuals(X_train, X_test, y_train, y_test):
-    model_log = sm.OLS(y_train, X_train)
-    results_log = model_log.fit_regularized(method='elastic_net', alpha=0.5, L1_wt=0.0)
-    hiv_resids_log = results_log.outlier_test()['student_resid']
+    model = sm.OLS(y_train, X_train)
+    results = model.fit()#_regularized(method='elastic_net', alpha=0.5, L1_wt=0.0)
+    hiv_resids = results.outlier_test()['student_resid']
     cols = list(X_train.columns)
     fig, axs = plt.subplots(len(X_train.columns), 1, figsize=(8,20))
     for idx, ax in enumerate(axs.flatten()):
-        ax.scatter(X_train[cols[idx]], hiv_resids_log)
+        ax.scatter(X_train[cols[idx]], hiv_resids)
         ax.hlines(0,
               X_train[cols[idx]].min(), 
               X_train[cols[idx]].max(), 
@@ -59,11 +57,44 @@ def plot_residuals(X_train, X_test, y_train, y_test):
         ax.set_xlabel(cols[idx])
         ax.set_ylabel('studentized residuals');
 
+def visualize_pred_residuals(X_train, X_test, y_train, y_test):
+    model = linear_model.Ridge(alpha = 0.05)
+    fitted = model.fit(X_train, y_train)
+    visualizer = ResidualsPlot(fitted)
+    pred = fitted.predict(X_test)
+    r = stats.linregress(pred, y_test)
+    print(r[2])
+    visualizer.fit(X_train, y_train)
+    visualizer.score(X_test, y_test)
+    visualizer.poof() 
+    cvr = model_selection.cross_validate(model, X_test, y_test, cv=10, return_train_score=True)
+    print('Training scores:', cvr['train_score'],'\n')
+    print('Testing scores:', cvr['test_score'])
+
+
+
 if __name__ == "__main__":
     hiv_data, X, y = read_data()
+    X["Log_Population"] = np.log(X['Population'])
+    #X["Log_HIVdiagnoses"] = np.log(X["HIVdiagnoses"])
+    X["HIVpop/Pop"] = X["PLHIV"] / X["Population"]
+    X.drop("Population", axis=1, inplace=True)
     y = np.log(y)
-    X_train_full, X_holdout, y_train_full, y_holdout = train_test_split(X, y, random_state=13)
-    model = linear_model.Ridge(alpha = 0.5)
+    X_train_full, X_holdout, y_train_full, y_holdout = train_test_split(X, y)
+    model = linear_model.Ridge(alpha = 0.05)
     fitted = model.fit(X_train_full, y_train_full)
-    results = fitted.predict(X_holdout)
-    l_results = stats.linregress(results, y_holdout) #[2] is r-value
+    #visualizer = ResidualsPlot(fitted)
+
+
+    #pred = fitted.predict(X_holdout)
+
+
+    #r = stats.linregress(pred, y_holdout)
+    #print(r[2])
+
+    #visualizer.fit(X_train_full, y_train_full)
+    #visualizer.score(X_holdout, y_holdout)
+    #visualizer.poof() 
+    #cvr = model_selection.cross_validate(model, X_holdout, y_holdout, cv=10, return_train_score=True)
+    #print('Training scores:',cvr['train_score'],'\n')
+    #print('Testing scores:',cvr['test_score'])
